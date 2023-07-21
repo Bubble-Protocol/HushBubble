05b59f0db5af0035ad3575308d1db4c82819a2fe
Dave.Potter <dave@datonalabs.org>
2023-07-21 19:28:13 +0100

    create static build

--- config-overrides.js
+++ config-overrides.js
@@ -1,55 +1,33 @@
-// module.exports = function override(config) {
-
-//   // const alias = config.resolve.alias || {};
-//   // Object.assign(alias, {
-//   //   ws: 'global/WebSocket',
-//   //   "buffer": "buffer",
-//   // })
-//   // config.resolve.alias = alias;
-
-//   config.node = {
-//     ...config.node,
-//     Buffer: true,
-//   };
-
-//   const fallback = config.resolve.fallback || {};
-//     Object.assign(fallback, {
-//       // "crypto": require.resolve("crypto-browserify"),
-//       crypto: false,
-//       stream: false,
-//       http: false,
-//       https: false,
-//       net: false,
-//       tls: false,
-//       url: false,
-//       "buffer": require.resolve("buffer/"),
-//     })
-//     config.resolve.fallback = fallback;
-
-//     return config;
-// }
-
-
 const webpack = require('webpack'); 
 module.exports = function override(config) { 
-		const fallback = config.resolve.fallback || {}; 
-		Object.assign(fallback, { 
-    	"crypto": require.resolve("crypto-browserify"), 
-      "stream": require.resolve("stream-browserify"), 
-      "assert": require.resolve("assert"), 
-      "http": require.resolve("stream-http"), 
-      "https": require.resolve("https-browserify"), 
-      "os": require.resolve("os-browserify"), 
-      "url": require.resolve("url"),
-      net: false,
-      tls: false,
-      zlib: false,
-    }) 
-   config.resolve.fallback = fallback; 
-   config.plugins = (config.plugins || []).concat([ 
-   	new webpack.ProvidePlugin({ 
-    	process: 'process/browser', 
+
+  const fallback = config.resolve.fallback || {}; 
+
+  Object.assign(fallback, { 
+    "crypto": require.resolve("crypto-browserify"), 
+    "stream": require.resolve("stream-browserify"), 
+    "assert": require.resolve("assert"), 
+    "http": require.resolve("stream-http"), 
+    "https": require.resolve("https-browserify"), 
+    "os": require.resolve("os-browserify"), 
+    "url": require.resolve("url"),
+    net: false,
+    tls: false,
+    zlib: false,
+  }) 
+
+  config.resolve.fallback = fallback; 
+
+  config.plugins = (config.plugins || []).concat([ 
+    new webpack.ProvidePlugin({ 
+      process: 'process/browser', 
       Buffer: ['buffer', 'Buffer'] 
     }) 
-   ]) 
-   return config; }
+  ]) 
+
+  config.devServer = {
+    historyApiFallback: true,
+  };
+
+  return config; 
+}
