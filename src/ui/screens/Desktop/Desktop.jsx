e2df23c6deb522602c3641ac949207d0235dcb11
Dave.Potter <dave@datonalabs.org>
2023-07-23 12:05:18 +0100

    add about page

--- src/ui/screens/Desktop/Desktop.jsx
+++ src/ui/screens/Desktop/Desktop.jsx
@@ -20,6 +20,7 @@ export const Desktop = () => {
         <h1 className="title">HushBubble</h1>
         <div className="spacer" />
         {!online && <div className="connecting-indicator"><span>offline</span><span className="loader loader-small"></span></div> }
+        <a class="header-link" href="about.html">What is HushBubble?</a>
       </header>
 
       {/* Content */}
